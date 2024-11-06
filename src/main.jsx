fa072014b02242a476a16aac1f8913c090253c31
zulfuqarov <zulfuqarov.nebi@gmail.com>
2024-11-07 01:52:10 +0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// ELxan yazdi bura
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-// deyisdirdim 
