567f4e7b3177b4b00aea5cf612ee0b31ace7959a
zulfuqarov <zulfuqarov.nebi@gmail.com>
2024-11-07 01:08:29 +0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 // ELxan yazdi bura
+// testetsetstese
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
