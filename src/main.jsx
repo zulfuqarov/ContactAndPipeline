711b74bcefb4b43f3d9434a0bbeccdbd9a93ce0d
Elxan <elxanxanlarov@egmail.com>
2024-11-07 01:29:45 +0400

    sildim

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// ELxan yazdi bura
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
