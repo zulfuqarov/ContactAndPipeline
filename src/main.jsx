b23388cd49448759e5241f1a8ed2175912e28c05
Elxan <elxanxanlarov@egmail.com>
2024-11-07 01:10:51 +0400

    Elxan eledi

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// ELxan yazdi bura
+// ELxan yazdi ve indi de silinecek 100
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
